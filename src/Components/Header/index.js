4d6fe025be17baf7fc551899db883f2118c69330
SARANYA251096 <saranyaayyamuthu96@gmail.com>
2023-07-14 11:47:46 +0530

    updatedresponsiveness

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
       <h1>Portfolio</h1>
-      <nav className="navbar">
-        <Link to="/" style={{ "--i": 1 }} className="active">
+      <button className="menu-toggle" onClick={toggleNav}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      <nav className={`navbar ${isOpen ? "open" : ""}`}>
+        <Link
+          to="/"
+          className="nav-link"
+          style={{ "--i": 1 }}
+          onClick={toggleNav}
+        >
           Home
         </Link>
-        <Link to="/about" style={{ "--i": 2 }}>
+        <Link
+          to="/about"
+          className="nav-link"
+          style={{ "--i": 2 }}
+          onClick={toggleNav}
+        >
           About
         </Link>
-        <Link to="/skills" style={{ "--i": 3 }}>
+        <Link
+          to="/skills"
+          className="nav-link"
+          style={{ "--i": 3 }}
+          onClick={toggleNav}
+        >
           Skills
         </Link>
-        <Link to="/projects" style={{ "--i": 4 }}>
+        <Link
+          to="/projects"
+          className="nav-link"
+          style={{ "--i": 4 }}
+          onClick={toggleNav}
+        >
           Projects
         </Link>
-        <Link to="/contact" style={{ "--i": 5 }}>
+        <Link
+          to="/contact"
+          className="nav-link"
+          style={{ "--i": 5 }}
+          onClick={toggleNav}
+        >
           Contact
         </Link>
       </nav>
